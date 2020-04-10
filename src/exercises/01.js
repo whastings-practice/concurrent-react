0f8d8f2a3cc319c95a8cf9fe82ce9a1acf08305f
Will Hastings <will@objectdotcreate.net>
2020-04-10 12:23:20 -0700

    Display fallback with Suspense

--- src/exercises/01.js
+++ src/exercises/01.js
@@ -6,12 +6,12 @@ import React from 'react'
 import {PokemonDataView} from '../utils'
 // 🐨 you'll need to import the fetchPokemon function
 // 💰 here you go:
-// import fetchPokemon from '../fetch-pokemon'
+import fetchPokemon from '../fetch-pokemon'
 // 💰 use it like this: fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
 
 // you'll also need the ErrorBoundary component from utils
 // 💰 here you go:
-// import {ErrorBoundary} from '../utils'
+import {ErrorBoundary} from '../utils'
 // 💰 use it like this: <ErrorBoundary><SomeOtherComponents /></ErrorBoundary>
 
 // By default, all fetches are mocked so we can control the time easily.
@@ -26,20 +26,13 @@ import {PokemonDataView} from '../utils'
 
 // 🐨 create the following mutable variable references (using let):
 // pokemon, pokemonError, pokemonPromise
-
-// 💣 delete this now...
-const pokemon = {
-  name: 'TODO',
-  number: 'TODO',
-  attacks: {
-    special: [{name: 'TODO', type: 'TODO', damage: 'TODO'}],
-  },
-  fetchedAt: 'TODO',
-}
+let pokemon
+let pokemonError
 
 // We don't need the app to be mounted to know that we want to fetch the pokemon
 // named "pikachu" so we can go ahead and do that right here.
 // 🐨 assign the pokemonPromise variable to a call to fetchPokemon('pikachu')
+const pokemonPromise = fetchPokemon('pikachu').then((p) => pokemon = p)
 
 // 🐨 when the promise resolves, set the pokemon variable to the resolved value
 // 🐨 if the promise fails, set the pokemonError variable to the error
@@ -48,6 +41,11 @@ function PokemonInfo() {
   // 🐨 if pokemonError is defined, then throw it here
   // 🐨 if there's no pokemon yet, then throw the pokemonPromise
   // 💰 (no, for real. Like: `throw pokemonPromise`)
+  if (!pokemon) {
+    // This is the current Suspense API for telling Suspense that data is loading
+    // It's likely to change
+    throw pokemonPromise
+  }
 
   // if the code gets it this far, then the pokemon variable is defined and
   // rendering can continue!
@@ -69,22 +67,14 @@ function App() {
         🐨 Then wrap all that with an <ErrorBoundary /> to catch errors
         💰 I wrote the ErrorBoundary for you. You can take a look at it in the utils file if you want
       */}
-      <PokemonInfo />
+      <React.Suspense
+        // Fallback rendered when React intercepts thrown promise
+        fallback={<div>Loading...</div>}
+      >
+        <PokemonInfo />
+      </React.Suspense>
     </div>
   )
 }
-
-/*
-🦉 Elaboration & Feedback
-After the instruction, copy the URL below into your browser and fill out the form:
-http://ws.kcd.im/?ws=Concurrent%20React&e=Simple%20Data-fetching&em=
-*/
-
-////////////////////////////////////////////////////////////////////
-//                                                                //
-//                 Don't make changes below here.                 //
-// But do look at it to see how your code is intended to be used. //
-//                                                                //
-////////////////////////////////////////////////////////////////////
 
 export default App
